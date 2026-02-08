3ed50059ea4211a9ef463df5e5a306d053536ee4
TOKIBA7 <107519070+TorieKibaki@users.noreply.github.com>
2026-02-08 09:50:27 +0300

    Initialized Bird Script, Made Bird Animation

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,41 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private float jumpForce;
+    private bool jumping = false;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumping = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (jumping)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jumping = false;
+        }
+    }
+}
